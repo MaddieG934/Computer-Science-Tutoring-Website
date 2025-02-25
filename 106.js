e45690d4449cb592f9a0b9d75845abdf27e1facd
MaddieG934 <madelynwesthuyzen@gmail.com>
2025-02-25 14:39:44 -0800

    106 lesson page content responsive to 106.js

--- 106.js
+++ 106.js
@@ -1,6 +1,13 @@
 // Fetch data from data.json and display in html
-fetch('./data.json')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    });
+function displayLessonHeader() {
+    document.getElementById("lessonHeader").textContent = "Lorem ipsum dolor";
+}
+
+function displayLessonContent() {
+    document.getElementById("lessonContent").textContent = "Lorem ipsum dolor sit amet";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    displayLessonHeader();
+    displayLessonContent();
+});
