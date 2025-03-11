ac5133fe5b818462184c04acf987a17bedda0cc3
MaddieG934 <madelynwesthuyzen@gmail.com>
2025-03-11 14:17:15 -0700

    second attempt to display js data in html using javascript

--- 106.js
+++ 106.js
@@ -1,35 +1,27 @@
 // Fetch data from data.json and display in html
-let jsData = {};
+document.addEventListener("DOMContentLoaded", function () {
+    displayLesson();
+    console.log('content displayed');
+});
 
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        jsData = data;
-        console.log(jsData);
-
-        document.addEventListener("DOMContentLoaded", function () {
-            displayLessonHeader();
-            displayLessonContent();
+// Display object => lessons => second lesson (106) => header, content
+function displayLesson() {
+    let jsData = {};
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            jsData = data;
+            console.log(jsData);
+        })
+        .catch(error => {
+            console.error('Error fetching JSON data: ', error);
         });
-    })
-    .catch(error => {
-        console.error('Error fetching JSON data: ', error);
-    });
 
-// Display object => lessons => second lesson (106) => header
-function displayLessonHeader() {
-    let lesson = jsData[1].lessons.find(lesson => lesson.lessonID === 2);
+    let lesson = jsData[1].lessons[1];
+    console.log(lesson);
+
     if (lesson) {
         document.getElementById("lessonHeader").textContent = lesson.header;
-    } else {
-        console.error('Lesson not found.');
-    }
-}
-
-// Display object => lessons => second lesson (106) => content
-function displayLessonContent() {
-    let lesson = jsData[1].lessons.find(lesson => lesson.lessonID === 2);
-    if (lesson) {
         document.getElementById("lessonContent").textContent = lesson.content;
     } else {
         console.error('Lesson not found.');
