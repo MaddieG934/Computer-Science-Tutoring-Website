0779e6379668d3cd32afdd7ef971419c0a0ccac0
brittanyavila <britbrithello@gmail.com>
2025-04-07 13:23:17 -0700

    Update my code

---
+++ 105.js
@@ -0,0 +1,33 @@
+// Fetch data from data.json as an array of objects
+async function fetchData() {
+    const response = await fetch('./data.json');
+    const data = await response.json();
+    return data;
+}
+
+// Display object => lessons => second lesson (105) => header, content
+async function displayLesson() {
+    let jsData = await fetchData();
+    let lesson = jsData[1].members[1];
+
+    if (lesson) {
+       document.getElementById("lessonHeader").textContent = lesson.header;
+
+    } else {
+        console.error('Lesson not found.');
+    }
+}
+
+function goToQuiz() {
+    window.location.href = './105quiz.html';
+}
+
+function goToHome() {
+    window.location.href = './websitetest.html';
+}
+
+// On document load, display appropriate content
+document.addEventListener("DOMContentLoaded", function () {
+    displayLesson();
+    console.log('content displayed');
+});
--- 105quiz.js
+++ 105quiz.js
@@ -3,6 +3,23 @@ function goToQuiz() {
     window.open('./105quiz.html');
 }
 
+let qSolutionEls = []; // Store element ID's of the location of the solutions to each question on each page load
+
+// Determine the quiz score, switch the window, and display score
+function goToScore() {
+    let score = 0;
+
+    for (let i = 0; i < 5; i++) {
+        if (document.getElementById(qSolutionEls[i]).checked) {
+            score++;
+        }
+    }
+
+    window.location.href = './105score.html';
+
+    document.getElementById("scoreNum").innerHTML = score;
+}
+
 // Fetch data from data.json as an array of objects
 async function fetchData() {
     const response = await fetch('./data.json');
@@ -10,7 +27,7 @@ async function fetchData() {
     return data;
 }
 
-// Display object => question
+// Display object => question => prompt, optionA, optionB, optionC, solution
 async function populateQuiz() {
     let jsData = await fetchData();
     let questions = jsData[3].members;
@@ -25,7 +42,7 @@ async function populateQuiz() {
      *    lines has more details in a comment                                    *
      *                                                                           *
      *****************************************************************************/
-    let quizQuestions = questions.filter(question => question.quizID === 1); // quizID for intro to c++ is 1
+    let quizQuestions = questions.filter(question => question.quizID === 1); // 1 is quizID for intro to C++
 
     console.log(quizQuestions);
 
@@ -34,150 +51,34 @@ async function populateQuiz() {
 
         let indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-        // Pull question 1 from the data
-        let qIdx = indices[Math.floor(Math.random() * 10)];
-        let question = quizQuestions[qIdx];
-        indices.splice(indices.indexOf(qIdx), 1);
-
-        // Display prompt
-        document.getElementById("prompt_1").innerHTML = "1. " + question.prompt.replace(/\n/g, "<br />").replace(/\t/g, "&emsp;"); //change id here 
-
-        // Randomize the order of the answer options
-        let answers = [question.optionA, question.optionB, question.optionC, question.solution];
         for (let i = 0; i < 5; i++) {
-            // Swap 2 random elements in the answer list 5 times
-            let idx1 = Math.floor(Math.random() * 4);
-            let idx2;
-            do {
-                idx2 = Math.floor(Math.random() * 4);
-            } while (idx2 === idx1);
+            // Pull question from the data
+           let qIdx = indices[Math.floor(Math.random() * (10 - i))];
 
-            let temp = answers[idx1];
-            answers[idx1] = answers[idx2];
-            answers[idx2] = temp;
+            let question = quizQuestions[qIdx];
+            indices.splice(indices.indexOf(qIdx), 1);
+
+            // Display prompt
+            document.getElementById(`prompt_${i + 1}`).innerHTML = "1. " + question.prompt.replace(/\n/g, "<br />").replace(/\t/g, "&emsp;");
+
+            // Randomize the order of the answer options
+            let answers = [question.optionA, question.optionB, question.optionC, question.solution];
+            let optionElChars = ['A', 'B', 'C', 'D'];
+
+            for (let j = 4; j >= 1; j--) {
+                // Choose a random answer option
+                let optionIdx = (j > 1) ? Math.floor(Math.random() * j) : 0;
+
+                // Display that option in the next radio slot
+                document.getElementById(`${i + 1}_${optionElChars[4 - j]}`).innerHTML = answers[optionIdx];
+                answers.splice(optionIdx, 1);
+
+                // When the solution is picked as the next option, save its radio buttion ID
+                if (optionIdx === 3) {
+                    qSolutionEls.push(`${i + 1}${optionElChars[4 - j]}`);
+                }
+            }
         }
-
-        // Display data
-        document.getElementById("1_A").innerHTML = answers[0]; // SECOND CHANGE IS OVER HERE!!! Change the ID here to the ID of the LABEL for the first radio input (see 106quiz.html)
-        document.getElementById("1_B").innerHTML = answers[1]; // Do the same for all these lines, and for all similar lines for questions 2-5 under the "Display data" comments
-        document.getElementById("1_C").innerHTML = answers[2]; // STILL NEED TO CREATE ID 
-        document.getElementById("1_D").innerHTML = answers[3];
-
-        // Pull question 2 from the data
-        qIdx = indices[Math.floor(Math.random() * 9)];
-        question = quizQuestions[qIdx];
-        indices.splice(indices.indexOf(qIdx), 1);
-
-        // Display prompt
-        document.getElementById("prompt_2").innerHTML = "2. " + question.prompt.replace(/\n/g, "<br />").replace(/\t/g, "&emsp;");
-
-        // Randomize the order of the answer options
-        answers = [question.optionA, question.optionB, question.optionC, question.solution];
-        for (let i = 0; i < 5; i++) {
-            // Swap 2 random elements in the answer list 5 times
-            let idx1 = Math.floor(Math.random() * 4);
-            let idx2;
-            do {
-                idx2 = Math.floor(Math.random() * 4);
-            } while (idx2 === idx1);
-
-            let temp = answers[idx1];
-            answers[idx1] = answers[idx2];
-            answers[idx2] = temp;
-        }
-
-        // Display data
-        document.getElementById("2_A").innerHTML = answers[0]; //change id here 
-        document.getElementById("2_B").innerHTML = answers[1];
-        document.getElementById("2_C").innerHTML = answers[2];
-        document.getElementById("2_D").innerHTML = answers[3];
-
-        // Pull question 3 from the data
-        qIdx = indices[Math.floor(Math.random() * 8)];
-        question = quizQuestions[qIdx];
-        indices.splice(indices.indexOf(qIdx), 1);
-
-        // Display prompt
-        document.getElementById("prompt_3").innerHTML = "3. " + question.prompt.replace(/\n/g, "<br />").replace(/\t/g, "&emsp;"); //change id here
-
-        // Randomize the order of the answer options
-        answers = [question.optionA, question.optionB, question.optionC, question.solution];
-        for (let i = 0; i < 5; i++) {
-            // Swap 2 random elements in the answer list 5 times
-            let idx1 = Math.floor(Math.random() * 4);
-            let idx2;
-            do {
-                idx2 = Math.floor(Math.random() * 4);
-            } while (idx2 === idx1);
-
-            let temp = answers[idx1];
-            answers[idx1] = answers[idx2];
-            answers[idx2] = temp;
-        }
-
-        // Display data
-        document.getElementById("3_A").innerHTML = answers[0]; //change id here 
-        document.getElementById("3_B").innerHTML = answers[1];
-        document.getElementById("3_C").innerHTML = answers[2];
-        document.getElementById("3_D").innerHTML = answers[3];
-
-        // Pull question 4 from the data
-        qIdx = indices[Math.floor(Math.random() * 7)];
-        question = quizQuestions[qIdx];
-        indices.splice(indices.indexOf(qIdx), 1);
-
-        // Display prompt
-        document.getElementById("prompt_4").innerHTML = "4. " + question.prompt.replace(/\n/g, "<br />").replace(/\t/g, "&emsp;");
-
-        // Randomize the order of the answer options
-        answers = [question.optionA, question.optionB, question.optionC, question.solution];
-        for (let i = 0; i < 5; i++) {
-            // Swap 2 random elements in the answer list 5 times
-            let idx1 = Math.floor(Math.random() * 4);
-            let idx2;
-            do {
-                idx2 = Math.floor(Math.random() * 4);
-            } while (idx2 === idx1);
-
-            let temp = answers[idx1];
-            answers[idx1] = answers[idx2];
-            answers[idx2] = temp;
-        }
-
-        // Display data
-        document.getElementById("4_A").innerHTML = answers[0]; //change id here 
-        document.getElementById("4_B").innerHTML = answers[1];
-        document.getElementById("4_C").innerHTML = answers[2];
-        document.getElementById("4_D").innerHTML = answers[3];
-
-        // Pull question 5 from the data
-        qIdx = indices[Math.floor(Math.random() * 6)];
-        question = quizQuestions[qIdx];
-
-        // Display prompt
-        document.getElementById("prompt_5").innerHTML = "5. " + question.prompt.replace(/\n/g, "<br />").replace(/\t/g, "&emsp;"); //change here id 
-
-        // Randomize the order of the answer options
-        answers = [question.optionA, question.optionB, question.optionC, question.solution];
-        for (let i = 0; i < 5; i++) {
-            // Swap 2 random elements in the answer list 5 times
-            let idx1 = Math.floor(Math.random() * 4);
-            let idx2;
-            do {
-                idx2 = Math.floor(Math.random() * 4);
-            } while (idx2 === idx1);
-
-            let temp = answers[idx1];
-            answers[idx1] = answers[idx2];
-            answers[idx2] = temp;
-        }
-
-        // Display data
-        document.getElementById("5_A").innerHTML = answers[0]; //change here id 
-        document.getElementById("5_B").innerHTML = answers[1];
-        document.getElementById("5_C").innerHTML = answers[2];
-        document.getElementById("5_D").innerHTML = answers[3];
-
     } else {
         console.error('Questions not found.');
     }
@@ -188,4 +89,3 @@ document.addEventListener("DOMContentLoaded", function () {
     populateQuiz();
     console.log('content displayed');
 });
-
