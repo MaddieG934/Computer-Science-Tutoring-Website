81e2eb4280d387eb16e1e12ed53dc6133fb2bbb3
autumnrankin <autumn.rankin@biola.edu>
2025-04-19 00:40:09 -0700

    course completeness but don't merge it yet lol

---
+++ completionLevel.js
@@ -0,0 +1,91 @@
+// Navigate back to the home page
+function goToHome() {
+    window.location.href = './websitetest.html';
+}
+
+// Switch page to login page
+async function goToLogin() {
+    window.location.href = './login.html';
+}
+
+// Fetch data from data.json as an array of objects
+async function fetchData() {
+    const response = await fetch('./data.json');
+    const data = await response.json();
+    return data;
+}
+
+
+// Display login info
+async function displayLoginInfo() {
+    let jsData = await fetchData();
+    let loginCheck = jsData[5].members[0];
+
+    if (loginCheck.isLoggedIn) {
+        let users = jsData[0].members;
+        let matchingUser = users.filter(user => user.userID === loginCheck.userID);
+        let userName = matchingUser[0].userName;
+
+        document.getElementById("loginInfo").innerHTML = "Logged in as: " + `${userName}`;
+        document.getElementById("loginLink").textContent = "Logout";
+    } else {
+        document.getElementById("loginInfo").innerHTML = "Logged in as: Guest";
+        document.getElementById("loginLink").textContent = "Login";
+    }
+}
+
+// On click of "Logout", change 
+async function logout() {
+    let jsData = await fetchData();
+    let loginCheck = jsData[5].members[0];
+
+    if (loginCheck.isLoggedIn) {
+        loginCheck.isLoggedIn = 0;
+        loginCheck.userID = -1;
+    }
+}
+
+async function completeness(courseId, scoreNum) {
+
+
+    const percentage = (scoreNum/5) * 100; 
+
+    const courseElement = document.querySelector(`[data-course="${courseId}"]`); 
+
+    if (courseElement) {
+
+        courseElement.innerText = `${percentage}%`; 
+    }
+
+}
+
+// Display the quiz score just obtained
+async function populateScore() {
+    let jsData = await fetchData();
+    let user = jsData[0].members[0];
+
+    if (user) {
+
+        console.log("user found", user); 
+
+        completeness(105, user.max105Score);
+        completeness(106, user.max106Score);
+        completeness(220, user.max220Score);
+        completeness(230, user.max230Score);
+    } else {
+        error.log('User not found.');
+    }
+    
+}
+
+// On document load, display appropriate content
+document.addEventListener("DOMContentLoaded", function () {
+    displayLoginInfo();
+    populateScore();
+    console.log('content displayed');
+
+    document.getElementById("loginLink").addEventListener("click", function () {
+        logout();
+        goToLogin();
+    });
+});
